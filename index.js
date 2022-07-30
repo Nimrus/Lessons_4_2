05e04f5a99f496e343b862b28d2f8de00fe81159
Nimrus <59753998+Nimrus@users.noreply.github.com>
2022-07-30 10:52:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let number = 20;
+number = "test"
+console.log (number);
+
+let string = "num";
+string = 20 ;
+console.log (string);
+
+let boolean = true ;
+boolean = null;
+console.log (boolean);
